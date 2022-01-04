c1958456959b71ed336ec76585f46a31abeaf80d
Daeki <kimdaeki@gmail.com>
2022-01-04 17:32:57 +0900

    ad St3

--- src/com/iu/s1/student/StudentMain.java
+++ src/com/iu/s1/student/StudentMain.java
@@ -6,13 +6,17 @@ public class StudentMain {
 		// makeStudent 호출
 		// 이름, 번호, 국어, 영어, 수학 출력
 		StudentUtil studentUtil = new StudentUtil();
-		Student student  = studentUtil.makeStudent();
+		//Student student  = studentUtil.makeStudent();
+		Student [] students = studentUtil.makeStudents();
 		
-		System.out.println("이름 : "+student.name);
-		System.out.println("번호 : "+student.num);
-		System.out.println("국어 : "+student.kor);
-		System.out.println("영어 : "+student.eng);
-		System.out.println("수학 : "+student.math);
+		for(int i=0;i<students.length;i++) {
+			Student student = students[i];
+			System.out.println("이름 : "+student.name);
+			System.out.println("번호 : "+student.num);
+			System.out.println("국어 : "+student.kor);
+			System.out.println("영어 : "+student.eng);
+			System.out.println("수학 : "+student.math);
+		}
 		
 
 	}
