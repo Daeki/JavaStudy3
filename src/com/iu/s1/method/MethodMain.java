21703580b49a45627c57ee9b2245dcea2b9df7a6
Daeki <kimdaeki@gmail.com>
2022-01-04 15:34:09 +0900

    add Rs

--- src/com/iu/s1/method/MethodMain.java
+++ src/com/iu/s1/method/MethodMain.java
@@ -1,9 +1,26 @@
 package com.iu.s1.method;
 
+import java.util.Scanner;
+
 public class MethodMain {
 
 	public static void main(String[] args) {
 		System.out.println("MainMethod Start");
+		Scanner sc = new Scanner(System.in);
+		//객체 생성 코드
+		//new 클래스명();
+		ReturnStudy rs = new ReturnStudy();
+		
+		//참조변수명.멤버들
+		rs.test1();
+		
+		int count = rs.test2();
+		System.out.println("Main :"+count);
+		
+		String name = rs.test3();
+		System.out.println(name);
+		
+		Math.ra
 		
 		System.out.println("MainMethod Finish");
 	}
